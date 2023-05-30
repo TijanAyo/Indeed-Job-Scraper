01270561b9ad786df7974be0d235a156918bb16e
TijanAyo <ayomidetijani6@gmail.com>
2023-05-30 22:29:15 +0100

    Clean and modularized codebase

--- main.py
+++ main.py
@@ -2,65 +2,60 @@ from bs4 import BeautifulSoup as bs
 import requests
 import time
 
-# SOFTWARE DEVELOPER ROLE
-# POSTED - 14 DAYS AGO
-# PLATFORM - INDEED
-# LOCATION - UK
+def print_banner():
+    print("JobberBot")
 
 
-# DESIGN
-print(
-    """
-        JobberBot
-                :                
-               ;                
-              :                 
-              ;                 
-             /                  
-           o/                   
-         ._/\___,                
-             \                  
-             /                   
-             `     
-    """ 
-)
+def fetch_html(url):
+    response = requests.get(url)
+    return response.text
 
-time.sleep(3)
 
-base_url = 'https://uk.indeed.com'
-URL = f'https://uk.indeed.com/jobs?q=software+developer&l=london&sc=0kf%3Aattr%28DSQF7%29%3B&fromage=14'
-
-def Query_Jobs():
-
-    html_body = requests.get(URL).text
+def parse_jobs(html_body):
     soup = bs(html_body, 'lxml')
+    jobs = soup.find_all('div', class_='job_seen_beacon')
+    return jobs
 
-    jobs = soup.find_all('div', class_ = 'job_seen_beacon')
 
+def extract_job_details(job):
+    company_name = job.find('span', class_='companyName').text
+    location = job.find('div', class_='companyLocation').text
+    date_posted = job.find('span', class_='date').text
+    job_title = job.find('h2', class_='jobTitle').text
+    job_desc = job.find('div', class_='job-snippet').text
+    job_url = job.find('a', class_='jcs-JobTitle css-jspxzf eu4oa1w0').get('href')
+    job_url_link = f'{base_url}{job_url}'
+    return company_name, location, date_posted, job_title, job_desc, job_url_link
+
+
+def print_job_details(details):
+    company_name, location, date_posted, job_title, job_desc, job_url_link = details
+    print(f'Posted: {date_posted[6:]}')
+    print(f'Job Title: {job_title}')
+    print(f'Company Name: {company_name}')
+    print(f'Location: {location}')
+    print(f'More Info: {job_url_link}')
+    print(f'Description: {job_desc}')
+    print('\n')
+
+
+def query_jobs():
+    html_body = fetch_html(URL)
+    jobs = parse_jobs(html_body)
     for job in jobs:
-
-        company_name = job.find('span', class_ = 'companyName').text
-        location = job.find('div', class_ = 'companyLocation').text
-        #rating = job.find('span', class_='ratingsDisplay').text
-        date_posted = job.find('span', class_ = 'date').text
-        job_title = job.find('h2', class_= 'jobTitle').text
-        job_desc = job.find('div', class_ = 'job-snippet').text
-        job_url = job.find('a', class_ = 'jcs-JobTitle css-jspxzf eu4oa1w0').get('href')
-        job_url_link = f'{base_url}{job_url}'
-        
-
-        print(f'Posted:- {date_posted[6:]}')
-        print(f'Job Title:- {job_title}')
-        print(f'Company Name:- {company_name}')
-        print(f'Location:- {location}')
-        print(f'More Info:- {job_url_link}')
-        print(f'Description:- {job_desc}')
-        print('\n')
+        details = extract_job_details(job)
+        print_job_details(details)
 
 
-while True:
-    Query_Jobs()
-    print(""" \n
+# Constants
+base_url = 'https://uk.indeed.com'
+URL = 'https://uk.indeed.com/jobs?q=software+developer&l=london&sc=0kf%3Aattr%28DSQF7%29%3B&fromage=14'
+
+def main():
+    print_banner()
+    while True:
+        query_jobs()
+        print(""" \n
                         (
                      (  ) (
                       )    )
@@ -70,5 +65,9 @@ while True:
 (_______________________)
 
 REFRESHING... NEW BATCH COMING
-    """)
-    time.sleep(600)
+        """)
+        time.sleep(600)
+
+
+# Call the main function to execute the code
+main()
